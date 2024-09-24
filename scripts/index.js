18ad6ba572b92f6b4c6de187d7ac251f604e1702
Melevate <32_melevate@mail.ru>
2024-09-24 18:38:07 +0300

    Исправил

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,29 @@
 const container = document.querySelector('.content');
 const cardContainer = container.querySelector('.places__list');
 const addButton = container.querySelector('.profile__add-button');
+const cardTemplate=document.querySelector("#card-template").content;
+
 
 const addCard = (item, deleteFunc) => {
-  const cardTemplate=document.querySelector("#card-template").content;
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  const cardTitle = item.name;
-  const cardLink = item.link; 
-  cardElement.querySelector('.card__image').src = cardLink;
-  cardElement.querySelector('.card__image').alt = cardTitle;
-  cardElement.querySelector('.card__title').textContent = cardTitle;
-  
-  cardElement.querySelector(".card__delete-button").addEventListener("click", deleteFunc)
+  const cardElement = createCard(item, deleteFunc);
   cardContainer.append(cardElement);
 }
 
+const createCard = (item, deleteFunc) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = item.name;
+  const cardLink = item.link; 
+  cardImage.src = cardLink;
+  cardImage.alt = cardTitle;
+  cardElement.querySelector('.card__title').textContent = cardTitle;
+  cardElement.querySelector(".card__delete-button").addEventListener("click", deleteFunc);
+  return cardElement;
+}
 
-addButton.addEventListener('click', function () {
-  initialCards.forEach(item => addCard(item, deleteCard));
-});
+const deleteCard = event => event.target.closest(".places__item").remove();
+
+initialCards.forEach(item => addCard(item, deleteCard));
 
 
-const deleteCard = event => event.target.parentElement.remove();
 
