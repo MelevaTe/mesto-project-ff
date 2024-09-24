4532028f4fd16fd3d4bcf25d3ee92bc64605b908
Melevate <32_melevate@mail.ru>
2024-09-24 17:36:04 +0300

    Первая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const container = document.querySelector('.content');
+const cardContainer = container.querySelector('.places__list');
+const addButton = container.querySelector('.profile__add-button');
 
-// @todo: DOM узлы
+const addCard = (item, deleteFunc) => {
+  const cardTemplate=document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardTitle = item.name;
+  const cardLink = item.link; 
+  cardElement.querySelector('.card__image').src = cardLink;
+  cardElement.querySelector('.card__image').alt = cardTitle;
+  cardElement.querySelector('.card__title').textContent = cardTitle;
+  
+  cardElement.querySelector(".card__delete-button").addEventListener("click", deleteFunc)
+  cardContainer.append(cardElement);
+}
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+addButton.addEventListener('click', function () {
+  initialCards.forEach(item => addCard(item, deleteCard));
+});
 
-// @todo: Вывести карточки на страницу
+
+const deleteCard = event => event.target.parentElement.remove();
+
